07c997ce5a2b772d93c51522bfcffed59cae5850
Michael Meadow <techtutormm@gmail.com>
2023-08-12 03:31:30 +0000

    prep-javascript-functions

---
+++ prep-javascript-functions/prep-javascript-functions/main.js
@@ -0,0 +1,46 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+const addTwoNumbersResult = addTwoNumbers(2, 2);
+console.log("addTwoNumbers exercise:", addTwoNumbersResult)
+function convertHoursToMinutes(hours) {
+  return hours * 60
+}
+convert = convertHoursToMinutes(2);
+
+console.log(convert)
+
+function addAndMultiplyBy5(num1, num2) {
+  let result = (num1 + num2) * 5
+  console.log(result)
+}
+addAndMultiplyBy5(5, 10)
+
+function MultiplyAndDivideBy5(num1, num2) {
+  let result = (num1 * num2) / 5
+  console.log(result)
+}
+MultiplyAndDivideBy5(35, 10)
+
+function subtractTwoNumbers(num1, num2) {
+  let result = num1 - num2
+  console.log(result)
+}
+subtractTwoNumbers(22, 7)
+
+function getCircleCircumference(radius) {
+  let result = (radius * 2) * 3.14
+  console.log(result)
+}
+getCircleCircumference(5)
+
+function getFullName(firstName, lastName) {
+  var fullName = firstName + ' ' + lastName;
+  return fullName;
+}
+
+function cube(num1) {
+  let result = num1 ** 3
+  console.log(result)
+}
+cube(5)
